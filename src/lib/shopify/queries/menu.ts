c5ed2fe5ff6c676ed6f343a8cbc45c093bdc8ec1
hovo <adadad077@gmail.com>
2025-10-21 21:02:46 +0700

    frist

---
+++ src/lib/shopify/queries/menu.ts
@@ -0,0 +1,12 @@
+export const getMenuQuery = /* GraphQL */ `
+  query getMenu($handle: String!) {
+    menu(handle: $handle) {
+      items {
+        title
+        url
+      }
+    }
+  }
+`;
+
+
