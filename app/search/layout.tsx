7fe83502038c97c2c9fa771e58e4a58f5e3657b1
hovo <adadad077@gmail.com>
2025-10-28 22:31:57 +0700

    fix folder 3

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -9,14 +9,14 @@ export default function SearchLayout({
 }) {
   return (
     <>
-      <div className="mx-auto  flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row ">
+      <div className="mx-auto flex max-w-screen-2xl flex-col gap-3 md:gap-8 px-4 pb-4 text-black md:flex-row mt-5 md:mt-0">
         <div className="order-first w-full flex-none md:max-w-[125px] md:sticky md:top-28 self-start">
           <Collections />
         </div>
-        <div className="mt-7 order-last min-h-screen w-full md:order-0 md:overflow-y-auto md:max-h-[calc(100vh-5rem)] scrollbar-hide">
+        <div className="mt-7 order-last min-h-screen w-full md:order-0 md:overflow-y-auto scrollbar-hide">
           {children}
         </div>
-        <div className="order-0 flex-none md:order-last md:w-[125px] md:sticky md:top-28 self-start">
+        <div className="order-0 flex-none w-full md:order-last md:w-[125px] md:sticky md:top-28 self-start">
           <FilterList list={sorting} title="Sort by" />
         </div>
       </div>
--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -1,21 +1,21 @@
 import clsx from "clsx";
 import LogoIcon from "./icons/logo";
 
-export default function LogoSquare({ size }: { size?: "sm" | undefined }) {
+export default function LogoSquare({ size }: { size?: "md" | undefined }) {
   return (
     <div
       className={clsx(
         "flex flex-none items-center justify-center  ",
         {
-          "h-[50px] w-[140px] rounded-xl": !size,
-          "h-[30px] w-[30px] rounded-lg": size === "sm",
+          "h-[50px] w-[140px] rounded-xl": size === "md",
+          "h-[50px] w-[100px] rounded-lg": !size,
         }
       )}
     >
       <LogoIcon
         className={clsx({
-          "h-4 w-4": !size,
-          "h-2.5 w-2.5": size === "sm",
+          "h-4 w-4": size === "md",
+          "h-2.5 w-2.5": !size ,
         })}
       />
     </div>
