2c4720902655a5e20303ec7785d0d4e39aee04b6
Võ Hồ <voho@Vos-Mac-mini.local>
2025-11-08 00:52:50 +0700

    fix ui - mac

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,7 +2,7 @@ import { getMenu } from "../../lib/shopify";
 import HeaderClient from "./header/navbar-client-wrapper";
 
 export async function Navbar() {
-  const menu = await getMenu("menu-tea");
+  const menu = await getMenu("menu-header");
   return <HeaderClient menu={menu} />;
 }
 
