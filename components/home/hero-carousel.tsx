884f03cd89c13c6c4a133ac8eb9a7bfced7afc3b
hovo <adadad077@gmail.com>
2025-11-20 14:22:48 +0700

    fix hero banner 2

--- components/home/hero-carousel.tsx
+++ components/home/hero-carousel.tsx
@@ -130,7 +130,7 @@ export default function HeroCarousel() {
 
   return (
     <section
-      className="relative w-full overflow-hidden h-[60vh] md:h-screen cursor-grab active:cursor-grabbing"
+      className="relative w-full overflow-hidden h-[60vh] md:h-screen cursor-grab active:cursor-grabbing bg-black"
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
